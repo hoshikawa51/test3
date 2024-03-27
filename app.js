801510a78b450648c86e381744b7d68a719be1b2
hoshikawa51 <152587771+hoshikawa51@users.noreply.github.com>
2024-03-27 16:38:11 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const appVersion = '0.0.1';
+
+window.onload = (event) => {
+  document.getElementById('appVersion').innerHTML = appVersion;
+};
---
+++ sw.js
@@ -0,0 +1,5 @@
+var cacheName = 'pscv 0.0.4';
+
+self.addEventListener('fetch', function(e) {
+  //空でOK
+})
